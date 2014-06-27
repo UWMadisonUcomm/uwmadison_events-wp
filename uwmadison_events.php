0bb942460a0cad3ff64496fa6870ff9fa2e90725
Nick Weaver <jnweaver@wisc.edu>
2014-06-27 12:33:39 -0500

    Fix version

--- uwmadison_events.php
+++ uwmadison_events.php
@@ -1,13 +1,13 @@
 <?php
 /**
 * @package UwmadisonEvents
-* @version 1.1.4
+* @version 1.1.5
 */
 /*
 Plugin Name: UW-Madison Events
 Description: A wordpress plugin to interface with the UW-Madison events calendar (http://today.wisc.edu)
 Author: University Communications and Marketting at the University of Wisconsin-Madison
-Version: 1.1.4
+Version: 1.1.5
 */
 
 // Load the libraries
